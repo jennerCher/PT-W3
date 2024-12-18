e74cb337b70b1008f677abbc670e668176e0301f
Cheryl Jennerjohn <jennerjohnc0215@gmail.com>
2024-12-17 19:03:28 -0500

    second commit being made

---
+++ src/com/prominotech/Application.java
@@ -0,0 +1,14 @@
+package com.prominotech;
+
+public class Application {
+	public static void main(String[] args) {
+		//System.out.println("My first Java Program!");
+		// TODO Auto-generated method stub
+		int age =18;
+		System.out.println(age);
+		boolean ageExpression = age>=16;
+		System.out.println(ageExpression);
+		
+	}
+
+}
